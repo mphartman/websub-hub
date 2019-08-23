ae1cb4624a72009054573f809ce4ab6df80f3dbf
Michael Hartman <mphartman1@gmail.com>
2019-08-23 14:54:47 -0500

    Implement validation of subscriber intent

--- src/main/kotlin/hartman/websub/WebSubHub.kt
+++ src/main/kotlin/hartman/websub/WebSubHub.kt
@@ -1,5 +1,6 @@
 package hartman.websub
 
+import com.github.kittinunf.fuel.Fuel
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -26,7 +27,27 @@ class WebSubSubController {
     @PostMapping(consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun subscribe(@RequestParam subscriptionRequest: Map<String, String>): ResponseEntity<Any> {
         print(subscriptionRequest)
+        val callback = subscriptionRequest["hub.callback"].orEmpty()
+        val mode = subscriptionRequest["hub.mode"].orEmpty()
+        val topicUrl = subscriptionRequest["hub.topic"].orEmpty()
+        verifySubscriberIntent(callback, mode, topicUrl)
         return ResponseEntity.accepted().build()
     }
 
+    fun verifySubscriberIntent(callback: String, mode: String, topicUrl: String) {
+        val challenge = generateNewChallenge()
+        Fuel.get(callback, listOf("hub.mode" to mode, "hub.topic" to topicUrl, "hub.challenge" to challenge, "hub.lease_seconds" to "0"))
+                .response { request, response, result ->
+                    println(request)
+                    println(response)
+                    val (bytes, error) = result
+                    if (bytes != null) {
+                        println("[response bytes] ${String(bytes)}")
+                    }
+                }
+    }
+
+    fun generateNewChallenge() : String {
+        return java.util.UUID.randomUUID().toString()
+    }
 }
--- src/test/kotlin/hartman/websub/SubscriberControllerTests.kt
+++ src/test/kotlin/hartman/websub/SubscriberControllerTests.kt
@@ -1,27 +1,66 @@
 package hartman.websub
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.mockserver.client.MockServerClient
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.model.HttpRequest.request
+import org.mockserver.model.Parameter.param
+import org.mockserver.verify.VerificationTimes.exactly
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
 
 @WebMvcTest
 class SubscriberControllerTests(@Autowired val mockMvc: MockMvc) {
 
+    private lateinit var mockServer: ClientAndServer
+
+    @BeforeAll
+    fun startServer() {
+        mockServer = ClientAndServer.startClientAndServer(1080)
+    }
+
+    @AfterAll
+    fun stopServer() {
+        mockServer.stop()
+    }
+
     @Test
-    fun `Given a Subscriber When it POSTs a valid Subscription Request Then Hub should return Accepted`() {
+    fun `Given a Subscriber when it POSTs a valid Subscription Request then Hub should return Accepted`() {
         mockMvc.perform(
                 post("/hub")
-                        .characterEncoding( "utf-8")
+                        .characterEncoding("utf-8")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("hub.callback", "http://localhost/subscribers/foo/callback")
                         .param("hub.mode", "subscribe")
                         .param("hub.topic", "http://localhost/topics/foo"))
-                .andDo(print())
                 .andExpect(status().isAccepted)
+    }
+
+    @Test
+    fun `Given a Subscriber when it POSTs a valid Subscription Request then Hub should verify intent of Subscriber`() {
+        mockMvc.perform(
+                post("/hub")
+                        .characterEncoding("utf-8")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("hub.callback", "http://localhost:1080/a-fun-new-subscription")
+                        .param("hub.mode", "subscribe")
+                        .param("hub.topic", "http://localhost/topics/foo"))
+                .andReturn()
+
+        MockServerClient("localhost", 1080)
+                .verify(request()
+                        .withMethod("GET")
+                        .withPath("/a-fun-new-subscription")
+                        .withQueryStringParameters(
+                                param("hub.mode", "subscribe"),
+                                param("hub.topic", "http://localhost/topics/foo")
+                        ), exactly(1))
     }
 }
