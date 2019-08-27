db55db955c74020edf844a89b1b82d6d535af773
Michael Hartman <mphartman1@gmail.com>
2019-08-26 19:41:09 -0500

    fix test

--- src/test/kotlin/hartman/websub/WebSubHubIntegrationTests.kt
+++ src/test/kotlin/hartman/websub/WebSubHubIntegrationTests.kt
@@ -157,19 +157,6 @@ class WebSubHubIntegrationTests(@Autowired val mockMvc: MockMvc) {
 
     @Test
     fun `Given a Subscriber when it POSTs a valid unsubscribe request then Hub should return a 202 Accepted`() {
-        mockMvc.perform(
-                post("/hub")
-                        .characterEncoding("utf-8")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("hub.callback", "http://localhost/subscribers/bar/callback")
-                        .param("hub.mode", "unsubscribe")
-                        .param("hub.topic", "http://localhost/topics/bar"))
-                .andExpect(status().isAccepted)
-    }
-
-    @Test
-    fun `Given a Subscriber when it POSTs a unsubscribe request for a non-existent subscription then Hub should return a 404`() {
-
         MockServerClient("localhost", 1080)
                 .`when`(
                         request()
@@ -192,6 +179,18 @@ class WebSubHubIntegrationTests(@Autowired val mockMvc: MockMvc) {
                         .param("hub.topic", "http://localhost/topics/bar"))
                 .andExpect(status().isAccepted)
 
+        mockMvc.perform(
+                post("/hub")
+                        .characterEncoding("utf-8")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("hub.callback", "http://localhost/bazChanged")
+                        .param("hub.mode", "unsubscribe")
+                        .param("hub.topic", "http://localhost/topics/bar"))
+                .andExpect(status().isAccepted)
+    }
+
+    @Test
+    fun `Given a Subscriber when it POSTs a unsubscribe request for a non-existent subscription then Hub should return a 404`() {
         mockMvc.perform(
                 post("/hub")
                         .characterEncoding("utf-8")
